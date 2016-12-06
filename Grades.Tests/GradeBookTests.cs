6b80f6d9b3cb9df312f5a72d33b81dceddd30647
rquatela <rquatela@RJ>
2016-12-06 16:15:41 -0500

    Add project files.

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grades;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade((float)89.5);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(91, result.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade((float)89.5);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(75, result.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade((float)89.5);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade, 0.01);
+        }
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Welcome");
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade((float)89.5);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            Console.WriteLine("Press any key to continue...");
+            string hello = Console.ReadLine();
+            
+        }
+    }
+}
