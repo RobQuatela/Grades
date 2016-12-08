37fe8f777698c964f7e61468e936d5c1dfb06db2
rquatela <rquatela@RQDEVELOPER>
2016-12-08 04:27:53 -0500

    12/7/16

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -37,7 +37,33 @@ namespace Grades
             grades.Add(grade);
         }
 
-        public string Name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    if(_name != value)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;
+
+                        NameChanged(this, args);
+                    }
+
+                    _name = value;
+                }
+            }
+        }
+
+        public event NameChangedDelegate NameChanged;
+
+        private string _name;
         private List<float> grades;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -17,18 +17,41 @@ namespace Grades
             synth.Speak("Welcome");
 
             GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Rob's Gradebook";
+            book.Name = "Joe's Gradebook";
+            book.Name = "";
             book.AddGrade(91);
             book.AddGrade((float)89.5);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
 
             Console.WriteLine("Press any key to continue...");
             string hello = Console.ReadLine();
             
         }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
     }
 }
